acae28a2e770f768b9af99427a8576fca3b64e52
caner.demirci <caner.demirci@trendyol.com>
2022-01-21 23:22:35 +0300

    Project created.

---
+++ tests/HexagonalArchitecture.Test/UnitTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace HexagonalArchitecture.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
